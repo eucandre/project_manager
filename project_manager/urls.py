afc347fa15c8d9499d110ddc0b17f64af94277c9
Carlos Lima <eucandre@gmail.com>
2024-06-09 22:30:47 -0300

    Criação do index

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from app_core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('accounts/', include('app_user.urls')),
     path('projects/', include('app_project.urls'), name='projects'),
     path("organs/", include("app_orgaos.urls"), name="ogans"),
